9f0791e253d072dfbbb2f370e6d383f5954709d7
Dongsheng Zhang <armstrongzhang1989@gmail.com>
2016-09-11 20:31:30 -0700

    generate cpu for 1 sec

--- www/cpu_load.sh
+++ www/cpu_load.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-secs=3                      # Set interval (duration) in seconds.
+secs=1                      # Set interval (duration) in seconds.
 endTime=$(( $(date +%s) + secs )) # Calculate end time.
 
 while [ $(date +%s) -lt $endTime ]; do  # Loop until interval has elapsed.
