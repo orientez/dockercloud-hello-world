02f0374cd10109bc6c9f334b1d2fd6aceb00b380
orientez <armstrongzhang1989@gmail.com>
2016-11-03 19:14:20 +0000

    change to get stats

---
+++ scripts/get_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python 
+
+import math
+import sys
+f = open(sys.argv[1], 'r')
+all_lines = f.readlines()
+#print all_lines
+rtt_sum = 0.0
+num_list = []
+for line in all_lines:
+    val = long(line.rstrip())
+    num_list.append(val)
+    rtt_sum += val
+cnt = len(all_lines)
+print "avg: " + str(rtt_sum/cnt/10e9) + " cnt: " + str(cnt) + " min: " + str(min(num_list)/10e9) + " max: " + str(max(num_list)/10e9) 
