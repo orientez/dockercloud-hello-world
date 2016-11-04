c5796968597884af90a296cfddd3f0324d8f9f0a
orientez <armstrongzhang1989@gmail.com>
2016-11-04 23:43:48 +0000

    integrate experiment script

--- scripts/st_nb.sh
+++ scripts/st_nb.sh
@@ -1,7 +1,31 @@
-for i in {1..100}
-do 
-echo $i
-./rtt.sh $1 $3 &
-sleep $2; 
+#!/bin/bash
+
+# calculate the rtt for each curl resposne
+function rtt {
+    start=`date +%s%N`
+    curl -s http://localhost:"${1}" >> /tmp/response_"${2}"
+    end=`date +%s%N`
+
+    runtime=$((end-start))
+    echo $runtime >> "${2}"
+}
+
+# single running of experiments
+function single_run {
+    for i in {1..100000}
+    do 
+    	# echo $i
+    	rtt $1 $3 &
+    	sleep $2; 
+    done
+}
+
+for nrun in {1..6}
+do
+    echo "run ${nrun}"
+    single_run $1 $2 "${3}_${nrun}.log"
+    sleep 120
 done
+
+
 
